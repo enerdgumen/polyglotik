da78c88f6da79870cd1e0cb23447d6adc86e58e5
Mauro Rocchi <maurocchi@gmail.com>
2020-03-25 21:15:27 +0100

    refactor: add index to git

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// This file is auto-generated
+
+import { ToolDefinitions } from "./definitions";
+
+const definitions: ToolDefinitions = {
+    node: {
+        path: "./tools/node.ts",
+        className: "Node"
+    },
+    npm: {
+        path: "./tools/node.ts",
+        className: "Npm"
+    },
+    npx: {
+        path: "./tools/node.ts",
+        className: "Npx"
+    },
+    yarn: {
+        path: "./tools/node.ts",
+        className: "Yarn"
+    },
+    ruby: {
+        path: "./tools/ruby.ts",
+        className: "Ruby"
+    },
+    bundle: {
+        path: "./tools/ruby.ts",
+        className: "Bundle"
+    }
+};
+
+export default definitions;
