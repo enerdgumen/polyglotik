1ac345cd5bb0c8c1718c548bbdf3d8178a8b836a
Mauro Rocchi <maurocchi@gmail.com>
2020-03-25 21:43:08 +0100

    feat: add ruby tool

---
+++ src/tools/ruby.ts
@@ -0,0 +1,43 @@
+import { inject, injectable } from "tsyringe";
+import { Engine } from "../engine";
+import { Project } from "../project";
+import { Tool } from "./tool";
+
+@injectable()
+export class Ruby implements Tool {
+    command = "ruby";
+
+    constructor(
+        @inject("Engine") private engine: Engine,
+        @inject("Project") private project: Project
+    ) {}
+
+    async run(args: string[]) {
+        const { name } = this.project;
+        const { version } = this.project.options("ruby");
+        const container = await this.engine
+            .newContainer(`polyglotik/${name}/ruby`, {
+                name: "ruby",
+                tag: version
+            })
+            .useHostNetwork()
+            .useHostUser()
+            .useHostWorkingDir()
+            .start(this.command, args);
+        const statusCode = await container.wait();
+        await container.remove();
+        return statusCode;
+    }
+}
+
+@injectable()
+export class Bundle extends Ruby {
+    command = "bundle";
+
+    constructor(
+        @inject("Engine") engine: Engine,
+        @inject("Project") project: Project
+    ) {
+        super(engine, project);
+    }
+}
