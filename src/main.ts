fdaf3953a14c227711842e37ceb958c2b63045cb
Mauro Rocchi <maurocchi@gmail.com>
2020-04-04 12:02:44 +0200

    refactor: use local node installation instead of the system one

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import { exec } from "./exec";
 
+function parseArgs(): string[] {
+    const args = process.argv.slice(2);
+    return args[0] === "polyglotik" ? args.slice(1) : args;
+}
+
+const args = parseArgs();
+if (args.length === 0) {
+    console.error("Missing command");
+    process.exit(102);
+}
 exec(process.argv.slice(2));
