044d496306010b70c710704c0df6343574c4fbf3
Mauro Rocchi <maurocchi@gmail.com>
2020-08-03 21:16:08 +0200

    fix: startup script

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 DIR=${DIR:-`dirname $0`}
 if [ -e "$DIR/node/node" ]; then
-  NODE_PATH="$DIR/node"
-  NODE_MODULES="$DIR/node/node_modules"
+  export NODE_PATH="$DIR/node"
+  export NODE_MODULES="$DIR/node/node_modules"
 elif [ -e "$DIR/node/bin/node" ]; then
-  NODE_PATH="$DIR/node/bin"
-  NODE_MODULES="$DIR/node/lib/node_modules"
+  export NODE_PATH="$DIR/node/bin"
+  export NODE_MODULES="$DIR/node/lib/node_modules"
 else
   echo "Local node.js not found"
   exit 1
